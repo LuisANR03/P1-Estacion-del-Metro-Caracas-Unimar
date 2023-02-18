5553ce62290c2edddf6854835eda4415dc1cac8a
LuisANR03 <124104213+LuisANR03@users.noreply.github.com>
2023-02-18 12:22:54 -0400

    Add files via upload
    Inicio de programa

---
+++ procedure.pas
@@ -0,0 +1,24 @@
+program untitled;
+
+uses crt;
+var Nombre,Apellido,cobertura,Tipo,Color: string; 
+Cedula,CantBoletos,Precio,Dinero:integer;
+opcion,Comprar : string;
+
+BEGIN
+	writeln(' ------------------------------- ');
+	writeln('|       VENTA DE BOLETOS         |');
+	writeln('|  POR FAVOR INGRESE SUS DATOS   |');
+	writeln(' --------------------------------  ');
+	Writeln('Ingrese su Nombre');Readln(Nombre);
+	Writeln('Ingrese su Apellido');Readln(Apellido);
+	Writeln('Ingrese su Cedula');Readln(Cedula);
+	writeln(Nombre,' ',Apellido);
+	writeln(Cedula);
+	
+	
+	
+
+	
+END.
+
